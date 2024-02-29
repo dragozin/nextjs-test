53b2abf335e128577db36470fa368793d8c581b8
Dmitrii Ragozin <d-a-ragozin@yandex-team.ru>
2024-02-29 20:45:28 +0300

    c

--- app/og-video-page-without-redirect/layout.tsx
+++ app/og-video-page-without-redirect/layout.tsx
@@ -8,12 +8,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <title>Layouts Example</title>
-      <meta
-          property="og:video"
-          content="http://s3.mds.yandex.net/trailers/test.mp4"
-        />
-        <meta property="og:video:width" content="1920" />
-        <meta property="og:video:height" content="1080" />
+      <meta property='og:video' content='https://www.youtube.com/v/BQBjVr1iHH4' />
 
         <meta property="og:url" content="https://nextjs-test-bice-one.vercel.app/og-video-page-without-redirect"/>
         <meta property="og:image" content="https://avatars.mds.yandex.net/get-kinopoisk-image/4303601/868926a8-a3e4-4524-90ff-d0e4b1f1ed86/1200x630"/>
