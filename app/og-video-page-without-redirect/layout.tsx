69a702acb3405bbd2a110c51f66e18ce86171d96
Dmitrii Ragozin <d-a-ragozin@yandex-team.ru>
2024-03-01 12:43:46 +0300

    c

--- app/og-video-page-without-redirect/layout.tsx
+++ app/og-video-page-without-redirect/layout.tsx
@@ -9,13 +9,17 @@ export default function RootLayout({
     <html lang="en">
       <title>Layouts Example</title>
       <meta property="fb:app_id" content="121953784483000" data-tid="57f72b5"/>
-      <meta property='og:video' content='https://www.youtube.com/v/BQBjVr1iHH4' />
-
-        <meta property="og:url" content="https://www.kinopoisk.ru"/>
-        <meta property="og:image" content="https://avatars.mds.yandex.net/get-kinopoisk-image/4303601/868926a8-a3e4-4524-90ff-d0e4b1f1ed86/1200x630"/>
-        <meta property="og:title" content="My title"/>
-        <meta property="og:description" content="My description"/>
-        <meta property="og:type" content="video" />
+      <meta property="og:title" content="Watch Lupin | Netflix Official Site" />
+    <meta property="og:description"
+        content="Inspired by the adventures of Arsène Lupin, gentleman thief Assane Diop sets out to avenge his father for an injustice inflicted by a wealthy family." />
+    <meta property="og:url" content="https://www.netflix.com/il-en/title/80994082" id="meta-url" />
+    <meta property="og:image"
+        content="https://occ-0-1853-2773.1.nflxso.net/dnm/api/v6/E8vDc_W8CLv7-yMQu8KMEC7Rrr8/AAAABeanC00dirwbOI_qCf6NNgCCWsEJMO_opu469SFL2YHydczzuEZi16DBi10uzPnaqrrMZy6msFfqHk_6w0a5FAN7SA5XXPY2Ww5b.jpg?r=796" />
+    <meta property="og:video"
+        content="https://occ-0-1853-2773.1.nflxso.net/so/soa2/817/05f6317116f033d02737cd79408a9a2c.mp4?v=1" />
+    <meta property="og:video:secure_url"
+        content="https://occ-0-1853-2773.1.nflxso.net/so/soa2/817/05f6317116f033d02737cd79408a9a2c.mp4?v=1" />
+    <meta property="og:video:type" content="video/mp4" />
 
       <body>{children}</body>
     </html>
