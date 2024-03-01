2917bfe18e002ae9bfcdb6244f5687336d0b354f
Dmitrii Ragozin <d-a-ragozin@yandex-team.ru>
2024-03-01 13:15:16 +0300

    c

--- app/og-video-page-without-redirect/layout.tsx
+++ app/og-video-page-without-redirect/layout.tsx
@@ -16,9 +16,9 @@ export default function RootLayout({
       <meta property="og:image"
           content="https://avatars.mds.yandex.net/get-kinopoisk-image/4303601/868926a8-a3e4-4524-90ff-d0e4b1f1ed86/1200x630" />
       <meta property="og:video"
-          content="https://s3.mds.yandex.net/trailers/video_original/0ba6ad7048ddcd9ca67ba5c362a86fc8.mp4" />
+          content="https://s3.mds.yandex.net/trailers/youtubedl/4a1aba9827264271a4a070f9ba89b634/5e13d946-eea2-404b-b878-8e40858e1b2a/external/video.mp4" />
       <meta property="og:video:secure_url"
-          content="https://s3.mds.yandex.net/trailers/video_original/0ba6ad7048ddcd9ca67ba5c362a86fc8.mp4" />
+          content="https://s3.mds.yandex.net/trailers/youtubedl/4a1aba9827264271a4a070f9ba89b634/5e13d946-eea2-404b-b878-8e40858e1b2a/external/video.mp4" />
       <meta property="og:video:type" content="video/mp4" />
 
       <body>{children}</body>
