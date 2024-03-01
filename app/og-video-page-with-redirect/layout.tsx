59a403b21f6b3d8daf2ffeb58cd49a244b22dad9
Dmitrii Ragozin <d-a-ragozin@yandex-team.ru>
2024-03-01 13:38:21 +0300

    c

--- app/og-video-page-with-redirect/layout.tsx
+++ app/og-video-page-with-redirect/layout.tsx
@@ -7,16 +7,20 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <title>Layouts Example</title>
-      <meta
-          property="og:video"
-          content="https://nextjs-test-bice-one.vercel.app/video-page"
-        />
-        <meta property="og:video:type" content="video/mp4" />
-        <meta property="og:video:width" content="1920" />
-        <meta property="og:video:height" content="1080" />
+      <title>Video page with redirect</title>
+      <meta property="fb:app_id" content="121953784483000" data-tid="57f72b5"/>
+      <meta property="og:title" content="My title" />
+      <meta property="og:description"
+          content="My description" />
+      <meta property="og:url" content="https://kinopoisk.ru/film/326/"/>
+      <meta property="og:image"
+          content="https://avatars.mds.yandex.net/get-kinopoisk-image/4303601/868926a8-a3e4-4524-90ff-d0e4b1f1ed86/1200x630" />
+    <meta property="og:video"
+        content="https://nextjs-test-bice-one.vercel.app/video-page" />
+    <meta property="og:video:secure_url"
+        content="https://nextjs-test-bice-one.vercel.app/video-page" />
+      <meta property="og:video:type" content="video/mp4" />
 
-      <body>{children}</body>
-    </html>
+      <body>{children}</body>    </html>
   )
 }
--- app/video-page/page.tsx
+++ app/video-page/page.tsx
@@ -2,7 +2,7 @@ import { permanentRedirect, RedirectType } from 'next/navigation'
 
 
 const VideoPage = () => {
-    permanentRedirect('http://s3.mds.yandex.net/trailers/test.mp4', RedirectType.replace)
+    permanentRedirect('https://occ-0-1853-2773.1.nflxso.net/so/soa2/817/05f6317116f033d02737cd79408a9a2c.mp4?v=1', RedirectType.replace)
 }
 
 export default VideoPage;
