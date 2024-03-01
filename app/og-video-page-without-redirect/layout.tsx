0d033e0666989a584b94e4094068e1909db60e99
Dmitrii Ragozin <d-a-ragozin@yandex-team.ru>
2024-03-01 13:19:16 +0300

    c

--- app/og-video-page-without-redirect/layout.tsx
+++ app/og-video-page-without-redirect/layout.tsx
@@ -15,10 +15,10 @@ export default function RootLayout({
       <meta property="og:url" content="https://kinopoisk.ru/film/326/"/>
       <meta property="og:image"
           content="https://avatars.mds.yandex.net/get-kinopoisk-image/4303601/868926a8-a3e4-4524-90ff-d0e4b1f1ed86/1200x630" />
-      <meta property="og:video"
-          content="https://s3.mds.yandex.net/trailers/youtubedl/4a1aba9827264271a4a070f9ba89b634/5e13d946-eea2-404b-b878-8e40858e1b2a/external/video.mp4" />
-      <meta property="og:video:secure_url"
-          content="https://s3.mds.yandex.net/trailers/youtubedl/4a1aba9827264271a4a070f9ba89b634/5e13d946-eea2-404b-b878-8e40858e1b2a/external/video.mp4" />
+    <meta property="og:video"
+        content="https://occ-0-1853-2773.1.nflxso.net/so/soa2/817/05f6317116f033d02737cd79408a9a2c.mp4?v=1" />
+    <meta property="og:video:secure_url"
+        content="https://occ-0-1853-2773.1.nflxso.net/so/soa2/817/05f6317116f033d02737cd79408a9a2c.mp4?v=1" />
       <meta property="og:video:type" content="video/mp4" />
 
       <body>{children}</body>
